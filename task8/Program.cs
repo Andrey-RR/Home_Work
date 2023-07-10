e3367b25063dcaa9ec7cf88e631a881023a638a5
andrey <13rred@gmail.com>
2023-07-11 01:42:34 +0300

    task8

---
+++ task8/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4 
+//8 -> 2, 4, 6, 8
+
+Console.WriteLine("введите число");
+int N = int.Parse(Console.ReadLine());
+
+int firsteven = 2;               // решил определить первое чётное как начальное
+
+if (N < 2) 
+
+{
+Console.Write("нет чисел");
+}
+
+else
+{
+     while (firsteven < N+1)
+     
+
+    {
+    Console.Write(firsteven + " ");
+    firsteven = firsteven + 2;
+    }
+}
